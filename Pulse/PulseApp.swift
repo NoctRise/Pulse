bfe1f054d4ef08611a3966e2a8e6331f1c143fbd
Andreas R <andy1103@hotmail.de>
2024-02-07 11:16:10 +0100

    Initial Commit

---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by Andi on 07.02.24.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
